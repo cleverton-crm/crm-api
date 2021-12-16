fc0b40c61316908d24c85e5d893580bbe15bf6e7
Alexander Hunter <icehuntmen@gmail.com>
2021-12-16 17:33:49 +0300

    Global types

--- src/controllers/profile.controller.ts
+++ src/controllers/profile.controller.ts
@@ -1,19 +1,20 @@
 import {
   Body,
   Controller,
-  HttpException,
+  Get,
   HttpStatus,
   Inject,
   Logger,
+  Patch,
   Post,
 } from '@nestjs/common';
 import { ClientProxy } from '@nestjs/microservices';
-import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import * as fs from 'fs';
 import { cyan } from 'cli-color';
-import { firstValueFrom } from 'rxjs';
-import { ProfileDto } from '../dto/profile.dto';
+import { ProfilePersonaDto } from '../dto/profile.dto';
 import { SendAndResponseData } from '../helpers/global';
+import { ReadmeDescription } from '../helpers/readme';
 
 @ApiTags('Profile')
 @Controller('profile')
@@ -27,15 +28,108 @@ export class ProfileController {
     console.log(this.profileServiceClient);
   }
 
+  /**
+   * Создание профиля пользователя
+   * @param profileData
+   */
   @Post('/')
   @ApiOperation({
     summary: 'Создание профиля пользователя',
-    description: fs.readFileSync('docs/profile/create.md').toString(),
+    description: ReadmeDescription('docs/profile/create.md'),
   })
-  async createPersona(@Body() profileData: ProfileDto) {
-    const response = SendAndResponseData(
+  @ApiResponse({ type: ProfilePersonaDto, status: HttpStatus.OK })
+  async createPersona(@Body() profileData: ProfilePersonaDto) {
+    const response = await SendAndResponseData(
       this.profileServiceClient,
       'profile:persona',
+      profileData,
+    );
+    this.logger.log(cyan(JSON.stringify(response)));
+    return response;
+  }
+
+  @Patch('/me/update')
+  @ApiOperation({
+    summary: 'Фото или аватар пользователя',
+    description: ReadmeDescription('docs/profile/update.md'),
+  })
+  @ApiResponse({ type: ProfilePersonaDto, status: HttpStatus.OK })
+  async updatePersona(@Body() profileData: ProfilePersonaDto) {
+    const response = await SendAndResponseData(
+      this.profileServiceClient,
+      'profile:update',
+      profileData,
+    );
+    this.logger.log(cyan(response));
+    return response;
+  }
+
+  /**
+   * Обновление или добавление информации о месте проживания
+   * @param profileData
+   */
+  @Patch('/me/update/location')
+  @ApiOperation({
+    summary: 'Данные о месте проживания',
+    description: ReadmeDescription('docs/profile/location.md'),
+  })
+  @ApiResponse({ type: ProfilePersonaDto, status: HttpStatus.OK })
+  async insertOrUpdateAddressPersona(@Body() profileData: ProfilePersonaDto) {
+    const response = await SendAndResponseData(
+      this.profileServiceClient,
+      'profile:address',
+      profileData,
+    );
+    this.logger.log(cyan(response));
+    return response;
+  }
+
+  /**
+   * Обновление или замена фотографии пользователя
+   * @param profileData
+   */
+  @Patch('/me/update/avatar')
+  @ApiOperation({
+    summary: 'Фото или аватар пользователя',
+    description: ReadmeDescription('docs/profile/avatar.md'),
+  })
+  @ApiResponse({ type: ProfilePersonaDto, status: HttpStatus.OK })
+  async updateAvatarPersona(@Body() profileData: ProfilePersonaDto) {
+    const response = await SendAndResponseData(
+      this.profileServiceClient,
+      'profile:avatar',
+      profileData,
+    );
+    this.logger.log(cyan(response));
+    return response;
+  }
+
+  @Patch('/me/update/socials')
+  @ApiOperation({
+    summary: 'Социальные сети пользователя',
+    description: ReadmeDescription('docs/profile/social.md'),
+  })
+  @ApiResponse({ type: ProfilePersonaDto, status: HttpStatus.OK })
+  async updateSocialPersona(@Body() profileData: ProfilePersonaDto) {
+    const response = await SendAndResponseData(
+      this.profileServiceClient,
+      'profile:address',
+      profileData,
+    );
+    this.logger.log(cyan(response));
+    return response;
+  }
+
+  @Get('/me')
+  @ApiOperation({
+    summary: 'Данные пользователя',
+    description: ReadmeDescription('docs/profile/profile.md'),
+  })
+  @ApiResponse({ type: ProfilePersonaDto, status: HttpStatus.OK })
+  async myDataPersona(@Body() profileData: ProfilePersonaDto) {
+    const response = await SendAndResponseData(
+      this.profileServiceClient,
+      'profile:address',
       profileData,
     );
     this.logger.log(cyan(response));
